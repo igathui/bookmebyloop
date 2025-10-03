2a5a22e20b2063d2be120ad8d60d189aa0f99c69
Ian Gathui <igathui@gmail.com>
2025-10-03 23:49:25 +0300

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/sections/Hero";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-items-center">
+    <section className="flex h-full min-h-screen flex-col items-center justify-items-center">
       <Hero />
     </section>
   );
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import logoImg from "@/assets/images/logo.png";
 
@@ -7,11 +5,9 @@ import { ArrowRight, ArrowUpRight } from "lucide-react";
 
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 interface HeroProps {
-  badge?: string;
-  heading: string;
-  description: string;
   buttons?: {
     primary?: {
       text: string;
@@ -22,15 +18,9 @@ interface HeroProps {
       url: string;
     };
   };
-  image: {
-    src: string;
-    alt: string;
-  };
 }
 
 export default function Hero({
-  badge = "🔖 Appointment Scheduling",
-  description = "A Web Application Concept for a Mini-App for the Loop Consumer Platform.",
   buttons = {
     primary: {
       text: "Sign Up",
@@ -47,38 +37,37 @@ export default function Hero({
       <div className="container">
         <div className="grid items-center gap-8 lg:grid-cols-2">
           <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
-            {badge && (
-              <Badge variant="outline">
-                {badge}
-                <ArrowUpRight className="ml-2 size-4" />
-              </Badge>
-            )}
+            <Badge variant="outline">
+              🔖 Appointment Scheduling
+              <ArrowUpRight className="ml-2 size-4" />
+            </Badge>
             <h1 className="my-6 text-4xl font-bold text-pretty lg:text-6xl">
               Welcome to
               <br />
               <span className="text-orange-600">BookMe! by Loop </span>
             </h1>
             <p className="text-muted-foreground mb-8 max-w-xl lg:text-xl">
-              {description}
+              A Web Application Concept for a Mini-App for the Loop Consumer
+              Platform.
             </p>
             <div className="flex w-full flex-col justify-center gap-2 sm:flex-row lg:justify-start">
-              {buttons.primary && (
-                <Button asChild className="w-full sm:w-auto">
-                  <a href={buttons.primary.url}>{buttons.primary.text}</a>
-                </Button>
-              )}
-              {buttons.secondary && (
-                <Button asChild variant="outline" className="w-full sm:w-auto">
-                  <a href={buttons.secondary.url}>
-                    {buttons.secondary.text}
-                    <ArrowRight className="size-4" />
-                  </a>
-                </Button>
-              )}
+              <Button asChild className="w-full sm:w-auto">
+                <Link href="/signup">Sign Up</Link>
+              </Button>
+              <Button asChild variant="outline" className="w-full sm:w-auto">
+                <Link href="/login">
+                  Log In
+                  <ArrowRight className="size-4" />
+                </Link>
+              </Button>
             </div>
           </div>
-          <div className="max-h-96 w-full items-center justify-items-center rounded-md object-cover opacity-50">
-            <Image src={logoImg} alt="hero image" className="h-72 w-72" />
+          <div className="max-h-96 w-full items-center justify-items-center rounded-md object-cover">
+            <Image
+              src={logoImg}
+              alt="hero image"
+              className="h-72 w-72 opacity-50"
+            />
           </div>
         </div>
       </div>
